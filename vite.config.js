b8f2c16c7fb49dcee0cb443e9ac5623680718638
pankaj2k9 <pkp2.me2k9@gmail.com>
2024-05-25 23:05:43 +0600

    removed vercel.json file

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,42 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { transformWithEsbuild } from 'vite'
 import restart from 'vite-plugin-restart'
-import { transformWithEsbuild } from 'vite' // Make sure to import transformWithEsbuild
 
-export default defineConfig({
+export default {
     root: 'src/',
     publicDir: '../public/',
-    plugins: [
-        restart({ restart: ['../public/**'] }),
+    plugins:
+    [
+        // Restart server on static/public file change
+        restart({ restart: [ '../public/**', ] }),
+
+        // React support
         react(),
+
+        // .js file support as if it was JSX
         {
             name: 'load+transform-js-files-as-jsx',
-            async transform(code, id) {
-                if (!id.match(/src\/.*\.js$/)) return null
+            async transform(code, id)
+            {
+                if (!id.match(/src\/.*\.js$/))
+                    return null
+
                 return transformWithEsbuild(code, id, {
                     loader: 'jsx',
                     jsx: 'automatic',
-                })
+                });
             },
         },
     ],
-    server: {
-        host: true,
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env)
+    server:
+    {
+        host: true, // Open to local network and display URL
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
     },
-    build: {
-        outDir: '../dist',
-        emptyOutDir: true,
-        sourcemap: true
-    }
-})
+    build:
+    {
+        outDir: '../dist', // Output in the dist/ folder
+        emptyOutDir: true, // Empty the folder first
+        sourcemap: true // Add sourcemap
+    },
+}
